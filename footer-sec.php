38b5fb4aae8986367a8d8196dfba2681c7ab5800
Shreeya C <shreeya@auburnsolutions.com>
2020-11-12 22:48:24 +0530

    Reponsive changes

--- footer-sec.php
+++ footer-sec.php
@@ -1,28 +1,28 @@
 <section>   
                   <div class="service-container aos-init aos-animate" data-aos="fade-up" data-aos-duration="1000">
                      <div class="row ml-0 mr-0">
-                        <div class="service  col-md-3 col-12">
+                        <div class="service  col-md-6 col-12">
                            <div class="icon" style="background-image: url(images/service-sprites8cf3.png);"></div>
                            <div class="content">
                               <h3>Free home delivery</h3>
                               <p>Provide free home delivery for all product over $100</p>
                            </div>
                         </div>
-                        <div class="service col-md-3 col-12">
+                        <div class="service col-md-6 col-12">
                            <div class="icon" style="background-image: url(images/service-sprites8cf3.png);"></div>
                            <div class="content">
                               <h3>Quality Products</h3>
                               <p>We ensure the product quality that is our main goal</p>
                            </div>
                         </div>
-                        <div class="service  col-md-3 col-12">
+                        <div class="service  col-md-6 col-12">
                            <div class="icon" style="background-image: url(images/service-sprites8cf3.png);"></div>
                            <div class="content">
                               <h3>3 Days Return</h3>
                               <p>Return product within 3 days for any product you buy</p>
                            </div>
                         </div>
-                        <div class="service col-md-3 col-12">
+                        <div class="service col-md-6 col-12">
                            <div class="icon" style="background-image: url( images/service-sprites8cf3.png);"></div>
                            <div class="content">
                               <h3>Online Support</h3>
